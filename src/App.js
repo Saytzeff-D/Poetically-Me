8fd7f77b07e31a5fed9ce0fe76395a7e41363647
David Atanda <ololadedavid15@gmail.com>
2023-07-18 19:56:14 +0100

    poem app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -11,6 +14,18 @@ function App() {
         <Route 
           path='/'
           element={<Home />}
+        />
+        <Route 
+          path='/join'
+          element={<Register />}
+        />
+        <Route 
+          path='/login'
+          element={<Login />}
+        />
+        <Route 
+          path='/poet-board'
+          element={<Dashboard />}
         />
       </Routes>
     </BrowserRouter>
