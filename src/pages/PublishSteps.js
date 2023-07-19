e30264b5c1de4143900da537102f21a0fd478492
David Atanda <ololadedavid15@gmail.com>
2023-07-19 23:48:36 +0100

    steps to publish

--- src/pages/PublishSteps.js
+++ src/pages/PublishSteps.js
@@ -1,18 +1,14 @@
 import React, { Fragment } from "react";
-import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
-import StepButton from '@mui/material/StepButton';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import StepLabel from '@mui/material/StepLabel';
 import Logo from '../assets/poetically-me.png'
-import UploadPoem from "../components/UploadPoem";
 
 const PublishSteps = ()=>{
     return (
         <Fragment>
-            <div className="d-flex justify-content-center mt-5 mx-md-0 mx-4">
-                <div className="col-md-7 col-lg-4 text-center">
+            <div className="d-flex justify-content-center mt-5">
+                <div className="col-md-4 text-center">
                     <img src={Logo} className="img-fluid" />
                     <p className="my-1 fw-bold fs-5">
                         Publish poem in 4 easy steps
@@ -20,7 +16,6 @@ const PublishSteps = ()=>{
                     <Stepper activeStep={0} alternativeLabel>
                         <Step key='Upload Poem'>
                             <StepLabel>Upload Poem</StepLabel>
-                            <UploadPoem />
                         </Step>
                         <Step key='Add Title'>
                             <StepLabel>Add Title</StepLabel>
