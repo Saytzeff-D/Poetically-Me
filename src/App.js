69f0873a33c84f1a0a9b26b2740febf1c9b936a7
David Atanda <ololadedavid15@gmail.com>
2023-07-19 18:10:13 +0100

    app routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Name from './components/Name';
+import Username from './components/Username';
+import Redirect from './pages/Redirect';
+import Country from './components/Country';
+import State from './components/State';
+import BestDescribe from './components/BestDescribe';
+import DateOFBirth from './components/DateOfBirth';
+import SocialConnection from './components/SocialConnection';
+import Password from './components/Password';
+import EmailVerification from './components/EmailVerification';
+import Info from './components/Info';
+import Publish from './pages/Publish';
+import HowToPublish from './pages/HowToPublish';
+import PublishSteps from './pages/PublishSteps';
 
 function App() {
   return (
@@ -13,19 +27,68 @@ function App() {
       <Routes>
         <Route 
           path='/'
-          element={<Home />}
+          element={<Redirect />}
         />
         <Route 
           path='/join'
           element={<Register />}
-        />
+        >
+          <Route 
+            path='/join/' 
+            element={<Name />} 
+          />
+          <Route 
+            path='/join/username' 
+            element={<Username />} 
+          />
+          <Route 
+            path='/join/country' 
+            element={<Country />} 
+          />
+          <Route 
+            path='/join/state' 
+            element={<State />} 
+          />
+          <Route 
+            path='/join/best-describe' 
+            element={<BestDescribe />} 
+          />
+          <Route 
+            path='/join/dateOfBirth' 
+            element={<DateOFBirth />} 
+          />
+          <Route 
+            path='/join/social' 
+            element={<SocialConnection />} 
+          />
+          <Route 
+            path='/join/password' 
+            element={<Password />} 
+          />
+          <Route 
+            path='/join/email-verification' 
+            element={<EmailVerification />} 
+          />
+          <Route 
+            path='/join/info' 
+            element={<Info />} 
+          />
+        </Route>
         <Route 
-          path='/login'
+          path='/publish'
+          element={<Publish />}
+        />
+        <Route
+          path='/login' 
           element={<Login />}
         />
         <Route 
-          path='/poet-board'
-          element={<Dashboard />}
+          path='/how-to-publish'
+          element={<HowToPublish />}
+        />
+        <Route 
+          path='/publish-steps'
+          element={<PublishSteps />}
         />
       </Routes>
     </BrowserRouter>
