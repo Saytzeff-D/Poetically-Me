b47a2ae759f55bb1ce7a2bb975a2e9fa4ce7bb29
David Atanda <ololadedavid15@gmail.com>
2023-07-20 18:29:05 +0100

    working on the dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,35 +1,100 @@
-import React, { Fragment, useState } from "react";
-import Tab from "react-bootstrap/Tab";
-import Tabs from "react-bootstrap/Tabs";
+import React, { Fragment, useEffect, useState } from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import Logo from '../assets/poetically-me.png'
+import SidebarList from "../components/SidebarList";
+import PoemBar from "../components/PoemBar";
+import { Outlet } from "react-router";
 
-const Dashboard = ()=>{
-    const [key, setKey] = useState('poems')
+const Dashboard = (props)=>{
+    const drawerWidth = 280
+    const [mobileOpen, setMobileOpen] = useState(false)
+    const { window } = props
+
+    useEffect(()=>{
+        console.log(window)
+    })
+    const handleDrawerToggle = ()=>{
+        setMobileOpen(!mobileOpen)
+    }
+    const drawer = (
+        <div className='bg-sidebar border-0'>
+            <Toolbar className='my-0'>
+                <img className="sideNavLogo" src={Logo} alt="poetically logo" />
+            </Toolbar>
+            <SidebarList />
+        </div>
+    )
+    const container = window !== undefined ? () => window().document.body : undefined;
+
     return (
         <Fragment>
-            <div className="bg-poet-board">
-                <div className="container py-5">
-                    <div className="d-flex justify-content-between">
-                    <a href="/" className="brand-text text-decoration-none fs-1 fw-light">
-                        PoeticallyMe
-                    </a>
-                        <p className="fw-light fs-2">
-                            User
-                        </p>
-                    </div>
-                    <hr />
-                    <Tabs id='profile-tab' variant='pills' className='my-4' activeKey={key} onSelect={(k)=>setKey(k)} >
-                        <Tab tabClassName="text-dark fs-6 fw-lighter" eventKey="poems" title="Poems">
-                            {/* <hr /> */}
-                        </Tab>
-                        <Tab tabClassName="text-dark fs-6 fw-lighter" eventKey="compose" title="Compose">
-                            {/* <hr /> */}
-                        </Tab>
-                        <Tab tabClassName="text-dark fs-6 fw-lighter" eventKey="publish" title="Publish">
-                            {/*  */}
-                        </Tab>
-                    </Tabs>
-                </div>
-            </div>
+            <Box sx={{display: 'flex'}}>
+                <CssBaseline />
+                <AppBar
+                    className='shadow-none mb-5'
+                    position="fixed"
+                    sx={{
+                    width: { md: `calc(100% - ${drawerWidth}px)` },
+                    ml: { md: `${drawerWidth}px` },
+                    }}
+                >
+                <Toolbar className='bg-white'>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { md: 'none' } }}
+                    >
+                        <i className="fa fa-bars text-dark"></i>
+                    </IconButton>
+                    <PoemBar />
+                </Toolbar>
+                </AppBar>
+                <Box
+                    component="nav"
+                    sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
+                >
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        ModalProps={{
+                            keepMounted: true, // Better open performance on mobile.
+                        }}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                        }}
+                        >
+                        {drawer}
+                    </Drawer>
+                    <Drawer
+                        variant="permanent"
+                        sx={{
+                            display: { xs: 'none', md: 'block' },
+                            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                        }}
+                        open
+                        >
+                        {drawer}
+                    </Drawer>
+                </Box>
+                <Box
+                component="main" 
+                sx={{ flexGrow: 1, p: 3, width: { md: `calc(100% - ${drawerWidth}px)` } }}
+                >
+                    <Outlet />
+                </Box>
+            </Box>
         </Fragment>
     )
 }
