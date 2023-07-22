7c34469f1e680ca337d586f629019ec380bcc0c3
David Atanda <ololadedavid15@gmail.com>
2023-07-22 10:10:36 +0100

    made some modifications

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,15 +2,24 @@ import React, { Fragment } from "react";
 import Header from "../components/Layouts/Header";
 import ProfileBg from '../assets/profile.png'
 import Footer from "../components/Layouts/Footer";
+import { useNavigate } from "react-router";
 
 const Profile = ()=>{
+    const navigate = useNavigate()
     return (
         <Fragment>
             <Header />
             <div>
                 <img src={ProfileBg} className="img-fluid" />
             </div>
-            <p>Me</p>
+            <p className="display-5 py-5 text-center">
+                This page is currently under development
+            </p>
+            <div className="d-flex justify-content-center my-5">
+                <button onClick={()=>navigate('/summary')} className="btn btn-next px-5 rounded-pill">
+                    Continue to Summary
+                </button>
+            </div>
             <Footer />
         </Fragment>
     )
