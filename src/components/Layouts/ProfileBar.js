8e9f3db6d39ff44e6f1ae32323a3667b5d06d300
David Atanda <ololadedavid15@gmail.com>
2023-07-25 16:00:34 +0100

    dropdown

--- src/components/Layouts/ProfileBar.js
+++ src/components/Layouts/ProfileBar.js
@@ -10,14 +10,19 @@ const ProfileBar = ()=>{
                     <img src={Logo} className="" height='70px' />
                 </div>
                 <div className="d-flex justify-content-between">
-                    <div className="mt-3">
+                    <div className="mt-3 d-md-block d-none">
                         <img src={require('../../assets/cart.png')} className="mx-3" height='25px' />
                     </div>
-                    <div className="mt-3">
+                    <div className="mt-3 d-md-block d-none">
                         <p className="mx-3"><i className="fa fa-bell text-dark fa-lg"></i></p>
                     </div>
-                    <div className="bg-poetical-orange user-pill text-white px-3 rounded-pill">
-                        Mike <img src={User} height='30px' className="ms-2" />
+                    <div className="dropdown cursor-pointer">
+                        <div className="bg-poetical-orange dropdown-toggle user-pill text-white px-3 py-3 rounded-pill" data-bs-toggle="dropdown" aria-expanded="false">
+                            Mike <img src={User} height='30px' className="ms-2" />
+                        </div> 
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="/">Logout</a></li>                            
+                        </ul>
                     </div>
                 </div>
             </div>
