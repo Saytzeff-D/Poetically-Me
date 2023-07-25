a9bcd44429502f651dd243058823d7ae352b9276
David Atanda <ololadedavid15@gmail.com>
2023-07-25 16:01:22 +0100

    my shop

---
+++ src/components/Profile/MyShop.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const MyShop = ()=>{
+    return(
+        <div className="container">
+            <p className="fs-4 fw-normal">
+                My Shop
+            </p>
+            <p className="fs-6 fw-light">
+                Poems and customized items you are selling are here
+            </p>
+            <hr />
+            <div>
+                <p className="text-center display-5 py-5">
+                    Nothing is here
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default MyShop
