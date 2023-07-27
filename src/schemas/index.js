ee92911b9d6a1ac749f2d63dd08d19acfb103d08
David Atanda <ololadedavid15@gmail.com>
2023-07-27 01:33:29 +0100

    added some schemas

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,7 +1,23 @@
 import * as yup from 'yup'
 
-export const loginSchema =
-    yup.object().shape({
-        email: yup.string().email('Invalid Email').required('Email is required'),
-        password: yup.string().required('Password is required')
-    })
+export const loginSchema = yup.object().shape({
+    email: yup.string().email('Invalid Email').required('Email is required'),
+    password: yup.string().required('Password is required')
+})
+
+export const emailSchema = yup.object().shape({
+    email: yup.string().email('Invalid Email').required('Email is required')
+})
+
+export const nameSchema = yup.object().shape({
+    firstName: yup.string().required('Required'),
+    lastName: yup.string().required('Required')
+})
+
+export const usernameSchema = yup.object().shape({
+    username: yup.string().required('Required')
+})
+
+export const bestDescribeSchema = yup.object().shape({
+    bestDescribe: yup.string().required('Choose an option')
+})
