2f82d8cebd0d55d938027b7d611a6f7906f62687
David Atanda <ololadedavid15@gmail.com>
2023-07-27 17:15:11 +0100

    all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import ApiReducer from "./api";
 import StepperReducer from './stepper';
+import UserReducer from "./user";
 
-const appReducer = combineReducers({ ApiReducer, StepperReducer })
+const appReducer = combineReducers({ ApiReducer, StepperReducer, UserReducer })
 
 export default appReducer
