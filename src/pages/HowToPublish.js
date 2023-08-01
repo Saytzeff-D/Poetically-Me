d497f5b340064ebae8d3922088e4e59bf9737b8c
David Atanda <ololadedavid15@gmail.com>
2023-08-01 16:30:58 +0100

    validation

--- src/pages/HowToPublish.js
+++ src/pages/HowToPublish.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Logo from '../assets/poetically-me.png'
 import { useNavigate } from "react-router";
+import { useFormik } from "formik";
+import { howToPublishSchema } from "../schemas";
+import { Alert } from "@mui/material";
 
 const HowToPublish = ()=>{
     const navigate = useNavigate()
+    const chooseList = (id)=>{
+        document.getElementById(id).click()
+    }
+    const formik = useFormik({
+        initialValues: {
+            how: ''
+        },
+        validationSchema: howToPublishSchema,
+        onSubmit: (values)=>{
+            if (values.how == 'Submit') {
+                navigate('/publish-steps')
+            } else {
+                navigate('/profile/my-poems')
+            }
+        }
+    })
     return (
         <div className="d-flex justify-content-center pt-md-5 pt-3 mt-md-4 mx-md-0 mx-4">
             <div className="col-md-5 col-lg-3 text-center animate__animated animate__slow animate__fadeIn">
@@ -15,19 +34,26 @@ const HowToPublish = ()=>{
                     You can publish a poem, update a book you have already submitted
                 </p>
                 <div>
-                    <div className="form-check border border-dark cursor-pointer py-2 px-5 rounded my-4">
-                        <input className="form-check-input cursor-pointer" type="radio" id="submit" />
+                    {
+                        formik.errors.how
+                        &&
+                        <Alert severity="error">
+                            {formik.errors.how}
+                        </Alert>
+                    }
+                    <div onClick={()=>chooseList('submit')} className="form-check border border-dark cursor-pointer py-2 px-5 rounded my-4">
+                        <input onChange={formik.handleChange} className="form-check-input cursor-pointer" name="how" type="radio" value={'Submit'} id="submit" />
                         <label className="form-check-label fs-6 cursor-pointer">
                             Submit a new poem
                         </label>
                     </div>
-                    <div className="form-check border border-dark cursor-pointer py-2 px-5 rounded my-4">
-                        <input className="form-check-input cursor-pointer" type="radio" id="update"/>
+                    <div onClick={()=>chooseList('update')} className="form-check border border-dark cursor-pointer py-2 px-5 rounded my-4">
+                        <input onChange={formik.handleChange} className="form-check-input cursor-pointer" name="how" type="radio" value={'Update'} id="update"/>
                         <label className="form-check-label fs-6 cursor-pointer">
                             Update poem
                         </label>
                     </div>
-                    <button onClick={()=>navigate('/publish-steps')} className="btn btn-next text-white py-2 w-100">
+                    <button type="submit" onClick={formik.handleSubmit} className="btn btn-next text-white py-2 w-100">
                         Continue
                     </button>
                 </div>
