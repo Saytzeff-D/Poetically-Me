32883dbcf8bb095d4c54a0f743b0ba4bdbc64726
David Atanda <ololadedavid15@gmail.com>
2023-08-01 16:30:25 +0100

    working on upload poems

--- src/components/Steppers/AddTitle.js
+++ src/components/Steppers/AddTitle.js
@@ -1,24 +1,62 @@
+import { useFormik } from 'formik';
 import React, { Fragment } from 'react';
 import { useDispatch } from 'react-redux';
+import { addTitleSchema } from '../../schemas';
+import { Alert } from '@mui/material';
 
 const AddTitle = ()=>{
     const dispatch = useDispatch()
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            poet: '',
+            desc: ''
+        },
+        validationSchema: addTitleSchema,
+        onSubmit: (values)=>{    
+            const { title, poet, desc } = values
+            let publish = JSON.parse(sessionStorage.getItem('publish'))
+            sessionStorage.setItem('publish', JSON.stringify({...publish, title, poet, desc}))
+            dispatch({type: 'nextStep', payload: 2})
+        }
+    })
     return(
         <Fragment>
             <form className='px-md-5 px-3'>
+                {
+                    formik.errors.title
+                    ?
+                    <Alert severity='error'>
+                        {formik.errors.title}
+                    </Alert>
+                    :
+                    formik.errors.poet
+                    ?
+                    <Alert severity='error'>
+                        {formik.errors.poet}
+                    </Alert>
+                    :
+                    formik.errors.desc
+                    ?
+                    <Alert severity='error'>
+                        {formik.errors.desc}
+                    </Alert>
+                    :
+                    ''
+                }
                 <div className='form-group my-4'>
-                    <input className='form-control border border-dark' placeholder='Title' />
+                    <input className='form-control border border-dark' onChange={formik.handleChange} onBlur={formik.handleBlur} name='title' placeholder='Title' />
                 </div>
-                <div className='form-group my-4'>
+                {/* <div className='form-group my-4'>
                     <input className='form-control border border-dark' placeholder='Subtitle' />
+                </div> */}
+                <div className='form-group my-4'>
+                    <input className='form-control border border-dark' onChange={formik.handleChange} onBlur={formik.handleBlur} name='poet' placeholder="Poet's name" />
                 </div>
                 <div className='form-group my-4'>
-                    <input className='form-control border border-dark' placeholder="Poet's name" />
+                    <textarea className='form-control border border-dark' onChange={formik.handleChange} onBlur={formik.handleBlur} name='desc' rows={3} placeholder='Description(at least 50 characters)'></textarea>
                 </div>
-                <div className='form-group my-4'>
-                    <textarea className='form-control border border-dark' rows={3} placeholder='Description(at least 50 characters)'></textarea>
-                </div>
-                <button onClick={()=>dispatch({type: 'nextStep', payload: 2})} className='btn btn-next py-2 w-100'>
+                <button type='submit' onClick={formik.handleSubmit} className='btn btn-next py-2 w-100'>
                     Continue
                 </button>
             </form>
--- src/components/Steppers/OtherDetails.js
+++ src/components/Steppers/OtherDetails.js
@@ -1,32 +1,130 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Info from './ModalBox';
+import { Backdrop, CircularProgress } from '@mui/material';
+import { useFormik } from 'formik';
+import { OtherDetailsSchema } from '../../schemas';
+import { Alert } from '@mui/material';
+import axios from 'axios';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { useSelector } from 'react-redux';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { useNavigate } from 'react-router';
 
 const OtherDetails = ()=>{
     const [open, setOpen] = useState(false)
-    const upload = (e)=>{
-        e.preventDefault()
-        setOpen(true)
+    const [backdrop, setBackdrop] = useState(false)
+    const api = useSelector(state=>state.ApiReducer.serverApi)
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
+    useEffect(()=>{            
+        if (sessionStorage.getItem('action') !== null && sessionStorage.getItem('action') == 'aboutPublishing') {
+            upload(JSON.parse(sessionStorage.getItem('publish')))
+        } else {
+            console.log('me')
+        }
+    }, [])
+    const formik = useFormik({
+        initialValues: {
+            publisherName: '',
+            ISBN: '',
+            publicationDate: ''
+        },
+        validationSchema: OtherDetailsSchema,
+        onSubmit: (values)=>{
+            let { publisherName, ISBN, publicationDate } = values            
+            console.log(values)
+            let publish = JSON.parse(sessionStorage.getItem('publish'))
+            const publishObj = {...publish, publisherName, ISBN, publicationDate: new Date(publicationDate).toLocaleDateString() }
+            sessionStorage.setItem('publish', JSON.stringify(publishObj))
+            upload(publishObj)
+        }
+    })
+    const upload = (publishObj)=>{
+        setBackdrop(true)
+            axios.post(
+                `${api}poem/upload`,
+                publishObj,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`,
+                        'Content-Type': 'application/json'
+                    }
+                }
+            ).then(res=>{
+                sessionStorage.removeItem('action')
+                setBackdrop(false)
+                console.log(res.data)
+            }).catch(err=>{
+                setBackdrop(false)
+                console.log(err)
+                if (err.response) {
+                    if (err.response.data.message == 'Token Expired' || 'Invalid Token') {
+                        sessionStorage.setItem('action', 'aboutPublishing')
+                        navigate('/login')
+                    } else {
+                        setError(err.response.data.message)
+                    }
+                } else {
+                    setError(err.message)
+                }
+            })
     }
     return(
         <Fragment>
-            <form className='px-3 px-md-5'>
+            <div className='px-3 px-md-5'>
+                {
+                    formik.errors.publisherName
+                    ?
+                    <Alert severity='error'>
+                        {formik.errors.publisherName}
+                    </Alert>
+                    :
+                    formik.errors.ISBN
+                    ?
+                    <Alert severity='error'>
+                        {formik.errors.ISBN}
+                    </Alert>
+                    :
+                    formik.errors.publicationDate
+                    ?
+                    <Alert severity='error'>
+                        {formik.errors.publicationDate}
+                    </Alert>
+                    :
+                    error !== ''
+                    &&
+                    <Alert severity='error'>
+                        {error}
+                    </Alert>
+                }
                 <div className='form-group my-4'>
-                    <input className='form-control border border-dark' placeholder='Publisher name' />
+                    <input onChange={formik.handleChange} onBlur={formik.handleBlur} name='publisherName' className='form-control border border-dark' placeholder='Publisher name' />
                 </div>
                 <div className='form-group my-4'>
-                    <input className='form-control border border-dark' placeholder='ISBN Number' />
+                    <input onChange={formik.handleChange} onBlur={formik.handleBlur} name='ISBN' className='form-control border border-dark' placeholder='ISBN Number' />
                 </div>
                 <div className='form-group mb-4'>
                     <label className='form-label'>
                         Original Publication Date
                     </label>
-                    <input type='date' className='form-control border border-dark' placeholder="Original Publication Date" />
+                    <input onChange={formik.handleChange} onBlur={formik.handleBlur} name='publicationDate' type='date' className='form-control border border-dark' placeholder="Original Publication Date" />
                 </div>
-                <button onClick={(e)=>upload(e)} className='btn btn-next py-2 w-100'>
+               {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                    label="Controlled picker"
+                    name='publicationDate'
+                    onChange={formik.handleChange}
+                    />
+               </LocalizationProvider> */}
+                <button type='submit' onClick={formik.handleSubmit} className='btn btn-next py-2 w-100'>
                     Upload Poem
                 </button>
-            </form>
+            </div>
             <Info open={open} />
+            <Backdrop color='#fff' open={backdrop}>
+                <span className='spinner-border text-white fs-7'></span>
+            </Backdrop>
         </Fragment>
     )
 }
