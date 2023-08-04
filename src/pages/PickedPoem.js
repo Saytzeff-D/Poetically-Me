007e8adfa7abe455c25ec440b45f9a35fb97b4dd
David Atanda <ololadedavid15@gmail.com>
2023-08-04 14:09:30 +0100

    now adding to cart

--- src/pages/PickedPoem.js
+++ src/pages/PickedPoem.js
@@ -1,40 +1,48 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import getSymbolFromCurrency from "currency-symbol-map";
+import { useNavigate } from "react-router";
 
 const PickedPoem = ()=>{
     const [book, setBook] = useState('')
+    const pickedBook = JSON.parse(sessionStorage.getItem('book'))
+    const navigate = useNavigate()
     useEffect(()=>{
-        setBook(JSON.parse(sessionStorage.getItem('book')))
-    })
+        pickedBook == null ? navigate('/home') : setBook(pickedBook)        
+    }, [])
+    const addToCart = ()=>{
+        let arr = []
+        const storedCart = JSON.parse(sessionStorage.getItem('cart'))
+        let newCart = storedCart == null ? [...arr, book] : [...storedCart, book]
+        sessionStorage.setItem('cart', JSON.stringify(newCart))
+        sessionStorage.removeItem('book')
+        navigate('/cart')
+    }
     return (
         <Fragment>
             <div className="container mt-5">
                 <p className="fs-4 pt-4">Buy Book <i className="fa fa-book"></i> </p>
                 <hr />
-                <div className="row w-100 mt-5">
-                    <div className="col-md-4">
+                <div className="row w-100 my-5 border-bottom">
+                    <div className="col-md-4 pb-3">
                         <div className="mb-4">
                             <img src={book.coverImage} />
                         </div>
-                        <span className="bg-poetical-orange py-2 px-4 rounded-pill text-white fs-5">
+                        <span className="bg-poetical-orange py-2 px-4 rounded-pill text-white fs-5 mb-5">
                            Buy | {getSymbolFromCurrency(book.baseCurrency) + book.price}
                         </span>
                     </div>
-                    <div className="col-md-8 border-start my-5 my-md-0">
-                        <div className="d-flex justify-content-end d-md-none">
-                            <Button className="btn-next fw-bold py-2">Add to Cart</Button>
+                    <div className="col-md-8 border-start my-5 my-md-0">                        
+                        <p>Title: <span className="fw-light">{book.title}</span></p>
+                        <p>Author: <span className="fw-light">{book.poet}</span></p>
+                        <p>Language: <span className="fw-light">{book.lang}</span></p>
+                        <p>Publisher: <span className="fw-light">{book.publisherName}</span></p>
+                        <p>ISBN: <span className="fw-light">{book.ISBN}</span></p>
+                        <p>Description: <span className="fw-light">{book.poem_desc}</span></p>
+                        <div className="d-flex justify-content-end">
+                            <Button onClick={addToCart} className="btn-next fw-bold py-2 px-4 border-0">Add to Cart</Button>
                         </div>
-                        <p>Title: {book.title}</p>
-                        <p>Author: {book.poet}</p>
-                        <p>Language: {book.lang}</p>
-                        <p>Publisher: {book.publisherName}</p>
-                        <p>ISBN: {book.ISBN}</p>
-                        <p>Description: {book.poem_desc}</p>
                     </div>
-                </div>
-                <div className="d-flex justify-content-end">
-                    <Button className="btn-next fw-bold py-2">Add to Cart</Button>
                 </div>
             </div>
         </Fragment>
