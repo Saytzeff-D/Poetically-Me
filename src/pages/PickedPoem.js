b3bbf4ee53ee396e04999e3c1053326e01dbd60b
David Atanda <ololadedavid15@gmail.com>
2023-08-04 09:53:09 +0100

    now showing the detailed info of a poem

--- src/pages/PickedPoem.js
+++ src/pages/PickedPoem.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Button } from "@mui/material";
+import getSymbolFromCurrency from "currency-symbol-map";
 
 const PickedPoem = ()=>{
     const [book, setBook] = useState('')
@@ -13,21 +14,23 @@ const PickedPoem = ()=>{
                 <hr />
                 <div className="row w-100 mt-5">
                     <div className="col-md-4">
-                        <img />
+                        <div className="mb-4">
+                            <img src={book.coverImage} />
+                        </div>
                         <span className="bg-poetical-orange py-2 px-4 rounded-pill text-white fs-5">
-                           Buy | $9,000
+                           Buy | {getSymbolFromCurrency(book.baseCurrency) + book.price}
                         </span>
                     </div>
                     <div className="col-md-8 border-start my-5 my-md-0">
                         <div className="d-flex justify-content-end d-md-none">
                             <Button className="btn-next fw-bold py-2">Add to Cart</Button>
                         </div>
-                        <p>Title</p>
-                        <p>Author</p>
-                        <p>Language</p>
-                        <p>Publisher</p>
-                        <p>ISBN</p>
-                        <p>Description</p>
+                        <p>Title: {book.title}</p>
+                        <p>Author: {book.poet}</p>
+                        <p>Language: {book.lang}</p>
+                        <p>Publisher: {book.publisherName}</p>
+                        <p>ISBN: {book.ISBN}</p>
+                        <p>Description: {book.poem_desc}</p>
                     </div>
                 </div>
                 <div className="d-flex justify-content-end">
