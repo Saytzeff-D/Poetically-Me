00c7a33b850579f78e543e63364f0291fdf090b3
David Atanda <ololadedavid15@gmail.com>
2023-08-05 19:06:44 +0100

    api

--- src/reducers/api.js
+++ src/reducers/api.js
@@ -1,7 +1,7 @@
 const initState = {
     countryApi: 'https://api.countrystatecity.in/v1/countries/',
-    serverApi: 'https://poetically-server.onrender.com/'    
-    // http://localhost:9090/
+    serverApi: 'https://poetically-server.onrender.com/',   
+    // serverApi: 'http://localhost:9090/'
 }
 
 const ApiReducer = (state=initState, action)=>{
