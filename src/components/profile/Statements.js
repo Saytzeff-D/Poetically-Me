9c233003cb8e6d74925070a5e15a66e6570b3962
David Atanda <ololadedavid15@gmail.com>
2023-08-06 06:45:00 +0100

    now showing all sales

--- src/components/profile/Statements.js
+++ src/components/profile/Statements.js
@@ -1,6 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import {useFormik} from 'formik'
+import * as yup from 'yup'
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { Alert, Button, LinearProgress } from "@mui/material";
 
 const Statements = ()=>{
+    const api = useSelector(state=>state.ApiReducer.serverApi)
+    const [statement, setStatement] = useState([])
+    const [error, setError] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [type, setType] = useState('')
+    const formik = useFormik({
+        initialValues: {
+            type: ''
+        },
+        validationSchema: yup.object().shape({
+            type: yup.string().required('Required   ')
+        }),
+        onSubmit: (values)=>{
+            console.log(values)
+            setIsLoading(true)
+            setType(values.type)
+            if (values.type == 'Purchases') {
+                axios.get(`${api}transaction/purchases`, {
+                    headers: {
+                        'authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`,
+                        'content-type': 'application/json'
+                    }
+                }).then(res=>{                    
+                    setIsLoading(false)
+                    setStatement(res.data.purchases)
+                }).catch(err=>{
+                    setIsLoading(false)
+                    !err.response ? setError(err.name) : setError(err.response.data.message)
+                })
+            } else {                
+                axios.get(`${api}transaction/sales`, {
+                    headers: {
+                        'authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`,
+                        'content-type': 'application/json'
+                    }
+                }).then(res=>{                    
+                    setIsLoading(false)
+                    setStatement(res.data.sales)
+                }).catch(err=>{
+                    setIsLoading(false)
+                    !err.response ? setError(err.name) : setError(err.response.data.message)
+                })
+            }
+        }
+    })
     return (
         <div className="container">
             <p className="fs-4 fw-normal">
@@ -12,14 +62,71 @@ const Statements = ()=>{
             <hr />
             <div>
                 <div className="form-group d-flex flex-sm-row flex-column">
-                    <select className="form-select mx-1">
-                        <option>Select Statement Type</option>
+                    <select name="type" onChange={formik.handleChange} className="form-select mx-1">
+                        <option value={''}>Select Statement Type</option>
                         <option>Purchases</option>
                         <option>Sales</option>
                     </select>
-                    <button className="btn btn-next px-2 mx-1 py-2 my-md-0 my-2">Retrieve</button>
+                    <Button disabled={isLoading} type="submit" onClick={formik.handleSubmit} className="btn btn-next px-4 fw-bold mx-1 py-2 my-md-0 my-2">Retrieve</Button>
                 </div>
             </div>
+            {
+                isLoading
+                ?
+                <LinearProgress color="secondary" className="my-4" />
+                :
+                statement.length !== 0
+                ?
+                (
+                    <div className="my-4">
+                        <p>{type}</p>
+                        <div className="table-responsive">
+                            <table className="table table-striped table-light table-borderless">
+                                <thead>
+                                    <tr>
+                                        <th>S/N</th>
+                                        <th>Transaction Reference</th>
+                                        <th>Poem Title</th>
+                                        <th>ISBN</th>
+                                        <th>Amount</th>
+                                        <th>Publisher</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        statement.map((each, i)=>(
+                                            <tr key={i}>
+                                                <td>{i+1}</td>
+                                                <td>{each.trx_ref}</td>
+                                                <td> {each.title} </td>
+                                                <td> {each.ISBN} </td>
+                                                <td> {each.price} </td>
+                                                <td> {each.publisherName} </td>
+                                                <td> {new Date(each.transact_at).toDateString()} </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                )
+                :
+                error !== ''
+                ?
+                <Alert severity="error" className="my-3">
+                    {error}
+                </Alert>
+                :
+                statement.length == 0 && type !== ''
+                ?
+                <Alert severity="info" className="my-3">
+                    You have no records of {type}
+                </Alert>
+                :
+                ''
+            }
         </div>
     )
 }
