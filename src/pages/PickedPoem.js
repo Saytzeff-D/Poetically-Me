8414017a3edb2bb563cc66e83c4982732a598f4e
David Atanda <ololadedavid15@gmail.com>
2023-08-07 17:56:54 +0100

    now adding to library

--- src/pages/PickedPoem.js
+++ src/pages/PickedPoem.js
@@ -1,14 +1,20 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { Alert, Button, Snackbar } from "@mui/material";
+import { Alert, Button, Dialog, DialogContent, DialogTitle, Snackbar } from "@mui/material";
 import getSymbolFromCurrency from "currency-symbol-map";
 import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const PickedPoem = ()=>{
+    const api = useSelector(state=>state.ApiReducer.serverApi)
+    const currentUser = useSelector(state=>state.UserReducer.userInfo)
     const [book, setBook] = useState('')
     const pickedBook = JSON.parse(sessionStorage.getItem('book'))
     const navigate = useNavigate()
+    const [dialogOpen, setDialogOpen] = useState(false)
     const [openSnackbar, setOpenSnackbar] = useState(false)
-    const [error, setError] = useState('')
+    const [message, setMessage] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
     useEffect(()=>{
         pickedBook == null ? navigate('/home') : setBook(pickedBook)        
     }, [])
@@ -29,7 +35,7 @@ const PickedPoem = ()=>{
                 sessionStorage.removeItem('book')
                 navigate('/cart')
             } else {
-                setError('Item already in cart')
+                setMessage('Item already in cart')
                 setOpenSnackbar(true)
             }
         }        
@@ -39,6 +45,24 @@ const PickedPoem = ()=>{
             return;
         }
         setOpenSnackbar(false)
+    }
+    const addToLibrary = ()=>{
+        let payload = { poem_id: book.poem_id, reader_id: currentUser.user_id }
+        if (currentUser.user_id == undefined) {
+            setDialogOpen(true)
+        } else {
+            setIsLoading(true)
+            axios.post(`${api}library/addLibrary`, payload).then(res=>{
+                setIsLoading(false)
+                setMessage('Added Successfully')
+                navigate('/profile/my-library')
+                setOpenSnackbar(true)
+            }).catch(err=>{
+                setIsLoading(false)
+                setOpenSnackbar(true)
+                !err.response ? setMessage(err.name) : setMessage(err.response.data.message)
+            })
+        }
     }
     return (
         <Fragment>
@@ -50,9 +74,16 @@ const PickedPoem = ()=>{
                         <div className="mb-4">
                             <img src={book.coverImage} />
                         </div>
-                        <Button className="bg-poetical-orange text-dark fw-bold py-2 px-4 border-0">
-                           {/* Buy | {getSymbolFromCurrency(book.baseCurrency) + book.price} */}
-                           Add to Library
+                        <Button onClick={addToLibrary} disabled={isLoading} className="bg-poetical-orange text-dark fw-bold py-2 px-4 border-0">                           
+                           {
+                            isLoading
+                            ?
+                            <>
+                                <span className="spinner-border spinner-border-sm"></span> <span className="ps-1">Please wait...</span>
+                            </>
+                            :
+                            <>Add to Library</>
+                           }
                         </Button>
                     </div>
                     <div className="col-md-8 border-start my-5 my-md-0">                        
@@ -69,9 +100,22 @@ const PickedPoem = ()=>{
                 </div>
             </div>
 
+            <Dialog open={dialogOpen} className="" maxWidth={'xs'} fullWidth={true} >
+                <DialogTitle className="text-dark fs-4 fw-light">
+                    You are not logged in...
+                </DialogTitle>
+                <DialogContent>
+                    <Button onClick={()=>{
+                        navigate('/login')
+                        sessionStorage.setItem('purpose', 'library')
+                    }} className="btn-next mx-1 fs-8 fw-bold rounded-0">Log In</Button>
+                    <Button onClick={()=>setDialogOpen(false)} className="btn-next mx-1 fs-8 fw-bold rounded-0">Close</Button>
+                </DialogContent>
+            </Dialog>
+
             <Snackbar open={openSnackbar} onClose={handleClose} autoHideDuration={4000} anchorOrigin={{horizontal: 'center', vertical: 'bottom'}} >
-                <Alert severity="error" onClose={handleClose}>
-                    {error}
+                <Alert severity={message.includes('Successfully') ? 'success' : 'error'} onClose={handleClose}>
+                    {message}
                 </Alert>
             </Snackbar>
         </Fragment>
