8b0b66f58c1b622e7cfdd9dc188ba95354a7c042
David Atanda <ololadedavid15@gmail.com>
2023-08-09 10:24:33 +0100

    printing and downloading poems

--- src/pages/PrintBook.js
+++ src/pages/PrintBook.js
@@ -12,6 +12,9 @@ const PrintBook = ()=>{
         const printBook = JSON.parse(sessionStorage.getItem('print'))
         printBook !== null ? setBook(printBook) : navigate('/home')
     }, [])
+    const printBook = (bookUrl)=>{
+        window.open(`https://docs.google.com/viewer?url=${bookUrl}`)
+    }
     return(
         <Fragment>
              <div className="container mt-5">
@@ -19,7 +22,7 @@ const PrintBook = ()=>{
                 <hr />
                 {
                     book.map((each, i)=>(
-                        <div className="row w-100 my-5 border-bottom">
+                        <div className="row w-100 my-5 border-bottom pb-2">
                             <div className="col-md-4 pb-3">
                                 <div className="mb-4">
                                     <img src={each.coverImage} />
@@ -32,10 +35,8 @@ const PrintBook = ()=>{
                                 <p>Publisher: <span className="fw-light">{each.publisherName}</span></p>
                                 <p>ISBN: <span className="fw-light">{each.ISBN}</span></p>
                                 <p>Description: <span className="fw-light">{each.poem_desc}</span></p>
-                                <div className="d-flex justify-content-end">
-                                    <a href={each.bookFile.split('upload')[0] + 'upload/fl_attachment:' + each.title + each.bookFile.split('upload')[1]} download={true}>
-                                        <Button className="btn-next fw-bold py-2 px-4 border-0">Print</Button>
-                                    </a>
+                                <div className="d-flex justify-content-end">                                    
+                                    <Button onClick={()=>printBook(each.bookFile)} className="btn-next fw-bold py-2 px-4 border-0">Print</Button>                                    
                                 </div>
                             </div>
                         </div>
